1b0359e3f3f95391e26ae6dc859e1fa646d674cf
k8y1b <56333166+k8y1b@users.noreply.github.com>
2019-11-30 18:31:47 -0800

    Add files via upload

--- vector.pl
+++ vector.pl
@@ -12,11 +12,11 @@ vector(Entries, Field, Dimension) :-
   len(Entries, Dimension),
   in_field(Entries, Field).
 
-% norm1(Vector, Norm) - succeeds if Norm is the 1-Norm of the vector Vector,
-%                       and if Vector is in fact a valid Vector.
-norm1(vector(Entries, Field, Dimension), Norm) :-
+% vdot(Vector, Vdot) - succeeds if Vdot is the dot product of the vector Vector,
+%                      and if Vector is in fact a valid Vector.
+vdot(vector(Entries, Field, Dimension), Vdot) :-
   vector(Entries, Field, Dimension),
-  sum_entries(Entries, Norm),
+  sum_entries(Entries, Vdot),
   in_field(Entries, Field).
 
 % vsum(V1, V2, Res) - succeeds if V1, V2, Res are valid vectors and V1+V2=Res.
@@ -47,3 +47,24 @@ norm2(vector([H|T], F, D), Norm) :-
 norm2(vector([H], _, 1), Norm) :- pow2(H, Norm).
   
 % vector_proj(V, W, Res) - succeeds if Res is the projection of V
+% W is a basis of dimension D. W is a matrix.
+vector_proj(V, [W|T], Res) :-  
+  dot_product(V,W,VdotW), 
+  scalar_product(W, VdotW, W1dotV),
+  vector_proj(V, T, Nextres), 
+  vsum(W1dotV, Nextres, Res).
+vector_proj(vector(E, F, D), [], vector(Zeros, F, D) :- zerovector(D, Zeros).
+
+zerovector(2, vector([0,0], F, 2)).
+zerovector(3, vector([0,0,0], F, 3)).
+
+
+zerovector(0, vector([],F,D)).
+zerovector(D, vector([0|Z], F, N)) :- zerovector(Dn, vector(Z, F, N)), Dn is D-1.
+
+dot_product(vector([H1|T1], F, D), vector([H2|T2], F, D), Res) :- 
+  dot_product(vector(T1, F, Dn), vector(T2, F, Dn), Nextres),
+  Res is H1 * H2 + Nextres.
+
+dot_product(vector([], F, D), vector([], F, D), 0).
+
